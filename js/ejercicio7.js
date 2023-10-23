2e730da3ab8ad4c76841572a9d5a6bc22fae8375
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-10-23 01:45:47 -0300

    se crea una nueva rama y se realiza el ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -3,3 +3,18 @@ Ejemplo:
 
 input: 15 , 3, 9                 Output: El 15 es el número más grande
 */
+
+let numero1 = parseInt(prompt('Ingrese un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+let numero3 = parseInt(prompt('Ingrese un tercer numero'));
+
+if(numero1 > (numero2) + (numero3)){
+    document.write((numero1) +' es mayor que los demás')
+}
+if(numero2 > (numero1) + (numero3)){
+    document.write((numero2) +' es mayor que los demás')
+}
+if(numero3 > (numero1) + (numero2)){
+    document.write((numero3) +' es mayor que los demás')
+}
+
