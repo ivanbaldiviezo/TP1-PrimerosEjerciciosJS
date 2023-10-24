03ee09a18457e1f89963beecebcec73109a20e60
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-10-24 00:41:02 -0300

    se realizo el ejercicio 8 y se modifico una parte de texto del ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -9,12 +9,12 @@ let numero2 = parseInt(prompt('Ingrese un segundo numero'));
 let numero3 = parseInt(prompt('Ingrese un tercer numero'));
 
 if(numero1 > (numero2) + (numero3)){
-    document.write((numero1) +' es mayor que los demás')
+    document.write('El ' + (numero1) +' es el número más grande')
 }
 if(numero2 > (numero1) + (numero3)){
-    document.write((numero2) +' es mayor que los demás')
+    document.write('El ' + (numero2) +' es el número más grande')
 }
 if(numero3 > (numero1) + (numero2)){
-    document.write((numero3) +' es mayor que los demás')
+    document.write('El ' + (numero3) +' es el número más grande')
 }
 
--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -4,3 +4,12 @@ Ejemplo:
 input: 10      Output: El 10 es divisible por 2.
 input: 15      Output: El 15 no es divisible por 2.
 */
+
+let numero = parseInt (prompt("Ingresa un número:"));
+
+if (numero % 2 === 0) {
+  document.write('El ' + (numero) + " es divisible por 2.");
+} 
+else {
+  document.write('El ' + (numero) + " no es divisible por 2.");
+}
