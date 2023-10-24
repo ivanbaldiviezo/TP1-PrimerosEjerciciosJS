da3d5502911f5eca069d4383c824407f647f24be
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-10-24 17:12:37 -0300

    se corrige el ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -4,12 +4,14 @@ Ejemplo:
 input: 15 , 3              Output: El 15 es el número más grande
 */
 
-let numero1 = parseInt(prompt('Ingrese un primer numero'));
-let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
 
-if(numero1 > numero2){
-    document.write(numero1 + ' es mayor a ' + numero2)
+if (numero1 > numero2) {
+  document.write("El " + numero1 + " es el número más grande");
+} else if (numero2 > numero1) {
+  document.write("El " + numero2 + " es el número más grande");
+} else {
+  document.write("Los números ingresados son iguales (" + numero2 + ")");
 }
-if(numero1 < numero2){
-    document.write(numero2 + ' es mayor a ' + numero1)
-}
+
