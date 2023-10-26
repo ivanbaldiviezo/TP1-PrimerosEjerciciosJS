95860d598e354d12f012d055245ce9b650dce93b
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-10-26 16:36:42 -0300

    Se realizo el ejercicio 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -4,3 +4,27 @@ Ejemplo:
 input: 20       Output: El 20 es divisible por 2 y por 5.
 input: 210      Output: El 210 es divisible por 2, por 3, por 5 y por 7.
 */
+
+let numero = parseInt(prompt("Ingresa un número entero:"));
+
+let divisores = ""; // Cadena para almacenar los divisores
+
+
+if (numero % 2 === 0) {
+    divisores += "2, ";
+}
+if (numero % 3 === 0) {
+    divisores += "3, ";
+}
+if (numero % 5 === 0) {
+    divisores += "5, ";
+}
+if (numero % 7 === 0) {
+    divisores += "7, ";
+}
+
+if (divisores !== "") {
+    document.write(`${numero} es divisible por: ${divisores}.`);
+} else {
+    document.write(`${numero} no es divisible por ninguno de los números 2, 3, 5 ni 7.`);
+}
